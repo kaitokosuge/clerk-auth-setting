22037d292e7cbad6cba2256e349c6a3d27bf2f39
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-12-08 15:57:20 +0900

    fix: delete user account button

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
-import { currentUser } from "@clerk/nextjs/server";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
 	const user = await currentUser();
 	console.log("userです", user);
-
+	const deleteUser = async () => {
+		const client = await clerkClient();
+		if (user === null) {
+			return;
+		}
+		await client.users.deleteUser(user.id);
+	};
 	return (
 		<div className="">
 			{user?.fullName}
 			<br />
 			{user?.id}
 			<div></div>
+			<div onClick={deleteUser}>削除</div>
 		</div>
 	);
 }
