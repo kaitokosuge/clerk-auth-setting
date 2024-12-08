90f3ed3237082086eb2d4c17a8709040a7df75df
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-12-08 19:42:07 +0900

    fix: delete component done

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,17 @@
-import { clerkClient, currentUser } from "@clerk/nextjs/server";
+import DeleteUser from "@/features/DeleteUser";
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
 	const user = await currentUser();
 	console.log("userです", user);
-	const deleteUser = async () => {
-		const client = await clerkClient();
-		if (user === null) {
-			return;
-		}
-		await client.users.deleteUser(user.id);
-	};
+
 	return (
 		<div className="">
 			{user?.fullName}
 			<br />
 			{user?.id}
 			<div></div>
-			<div onClick={deleteUser}>削除</div>
+			<DeleteUser user={user} />
 		</div>
 	);
 }
