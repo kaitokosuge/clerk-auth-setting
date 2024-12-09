0821d5fc512bf8bd781c7451d834e67e4af7770a
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-12-09 16:45:31 +0900

    rec: email transaction

--- app/api/callback/clerk/route.ts
+++ app/api/callback/clerk/route.ts
@@ -2,7 +2,7 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
-import { clerkClient } from "@clerk/nextjs/server";
+// import { clerkClient } from "@clerk/nextjs/server";
 
 export async function POST(req: Request) {
 	const SIGNING_SECRET = process.env.SIGNING_SECRET;
